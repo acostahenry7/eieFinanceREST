8dd3922bf106917433a9eb483efea919f41e6d94
acostahenry7 <73194577+acostahenry7@users.noreply.github.com>
2022-07-11 12:00:25 -0400

    changes-11-7-22-12h

--- src/server/dbconfig.js
+++ src/server/dbconfig.js
@@ -1,25 +1,9 @@
 //Conexión prara finance web
 
-// module.exports = {
-//   host: "10.202.5.39",
-//   user: "webadmin",
-//   password: "EDJKNOSOj6",
-//   db: "finance",
-//   dialect: "postgres",
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
-
-//Conexión para finance local
-
 module.exports = {
-  host: "localhost",
-  user: "postgres",
-  password: "postgres",
+  host: "10.202.5.39",
+  user: "webadmin",
+  password: "EDJKNOSOj6",
   db: "finance",
   dialect: "postgres",
   pool: {
@@ -29,3 +13,19 @@ module.exports = {
     idle: 10000,
   },
 };
+
+//Conexión para finance local
+
+// module.exports = {
+//   host: "localhost",
+//   user: "postgres",
+//   password: "postgres",
+//   db: "finance",
+//   dialect: "postgres",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
