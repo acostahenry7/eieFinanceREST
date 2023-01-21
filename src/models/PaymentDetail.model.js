2e528948ef7bb3caf086b20ccfa861e58f5a0c8e
Henry Acosta <acostahenry7@github.com>
2023-01-20 20:55:55 -0400

    fix-last-payment-state

--- src/models/PaymentDetail.model.js
+++ src/models/PaymentDetail.model.js
@@ -1,26 +1,39 @@
 module.exports = (sequelize, Sequelize) => {
-    const PaymentDetail = sequelize.define('payment_detail', {
-        payment_detail_id: {
-            primaryKey: true, 
-            type: Sequelize.STRING,
-            defaultValue: Sequelize.UUIDV4,
-            allowNull: false
-        },
-        amortization_id: {
-            type: Sequelize.STRING
-        },
-        payment_id: {
-            type: Sequelize.STRING
-        },
-        pay: {
-            type: Sequelize.STRING
-        }
-    },{
-        schema: 'public',
-        freezeTableName:  true,
-        underscored: true,
-        timestamps: false
-    })
+  const PaymentDetail = sequelize.define(
+    "payment_detail",
+    {
+      payment_detail_id: {
+        primaryKey: true,
+        type: Sequelize.STRING,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      amortization_id: {
+        type: Sequelize.STRING,
+      },
+      payment_id: {
+        type: Sequelize.STRING,
+      },
+      pay: {
+        type: Sequelize.DOUBLE,
+      },
+      pay_mora: {
+        type: Sequelize.DOUBLE,
+      },
+      paid_mora_only: {
+        type: Sequelize.BOOLEAN,
+      },
+      status_type: {
+        type: Sequelize.STRING,
+      },
+    },
+    {
+      schema: "public",
+      freezeTableName: true,
+      underscored: true,
+      timestamps: false,
+    }
+  );
 
-    return PaymentDetail
-}
+  return PaymentDetail;
+};
