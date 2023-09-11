9820e6b00e3cc981447a000a7c4c081a9072cce8
Henry Acosta <acostahenry7@github.com>
2023-09-10 20:21:51 -0400

    fix-offline-back-nochanges

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,7 +8,7 @@ const Outlet = db.outlet;
 const AppAccessControl = db.appAccessControl;
 const bcrypt = require("bcryptjs");
 
-const APPVERSION = "1.11";
+const APPVERSION = "1.12";
 
 let tries = 0;
 let lockedUsers = [];
