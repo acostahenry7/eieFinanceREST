8265c55636af4fd36ec353df56ea4e119c83e351
Henry Acosta <acostahenry7@github.com>
2023-12-18 03:58:51 -0400

    12-18-payment-addr

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,7 +8,7 @@ const Outlet = db.outlet;
 const AppAccessControl = db.appAccessControl;
 const bcrypt = require("bcryptjs");
 
-const APPVERSION = "1.18";
+const APPVERSION = "1.19";
 
 let tries = 0;
 let lockedUsers = [];
