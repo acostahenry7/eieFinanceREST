5b73b31233ef9bb9e65e1ac7f6b0fe3420fd2275
Henry Acosta <acostahenry7@github.com>
2024-03-24 11:53:40 -0400

    changes-2nd-march

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,7 +8,7 @@ const Outlet = db.outlet;
 const AppAccessControl = db.appAccessControl;
 const bcrypt = require("bcryptjs");
 
-const APPVERSION = "1.25";
+const APPVERSION = "1.26";
 
 let tries = 0;
 let lockedUsers = [];
